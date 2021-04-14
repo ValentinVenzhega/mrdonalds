3cb749e398fc5f87bba3292b42af6fa8f0a75d47
Valentin Venzhega <valek917@yandex.ru>
2021-04-14 19:18:46 +0300

    добавили отображение допов

--- src/Components/Order/OrderListItem.js
+++ src/Components/Order/OrderListItem.js
@@ -8,6 +8,7 @@ import { formatCurrency } from '../Functions/secondaryFunction';
 const OrderItemStyled = styled.li`
    display: flex;
    margin: 15px 0;
+   flex-wrap: wrap;
 `;
 
 const ItemName = styled.span`
@@ -34,11 +35,23 @@ const TrashButton = styled.button`
    cursor: pointer;
 `;
 
-export const OrderListItem = ({ order }) => (
+const Toppings = styled.div`
+   color: #9a9a9a;
+   font-size: 14px;
+   width: 100%;
+`;
+
+export const OrderListItem = ({ order }) => {
+   const topping = order.topping.filter(item => item.checked)
+   .map(item => item.name)
+   .join(', ');
+   
+   return(
    <OrderItemStyled>
       <ItemName>{order.name}</ItemName>
       <span>{order.count}</span>
       <ItemPrice>{formatCurrency(totalPriceItems(order))}</ItemPrice>
       <TrashButton/>
+      {topping && <Toppings>Допы: {topping} </Toppings>}
    </OrderItemStyled>
-);
+)};
