0c8461fa47b9248a6ac29a01005ff5ba2732ae54
Valentin Venzhega <valek917@yandex.ru>
2021-04-16 21:19:01 +0300

    добавили регистрацию

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { NavBar } from './Components/NavBar/NavBar';
 import { Menu } from './Components/Menu/Menu';
 import { GlobalStyle } from './Components/Style/GlobalStyle';
@@ -6,8 +8,23 @@ import { ModalItem } from './Components/Modal/ModalItem';
 import { Order } from './Components/Order/Order';
 import { useOpenItem } from './Components/Hooks/useOpenItem';
 import { useOrders } from './Components/Hooks/useOrders';
+import {useAuth } from './Components/Hooks/useAuth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDETRacRAXFxDCZmNwv5oTb_1SYEzOO_1E",
+  authDomain: "mrdonalds-99ed8.firebaseapp.com",
+  projectId: "mrdonalds-99ed8",
+  storageBucket: "mrdonalds-99ed8.appspot.com",
+  messagingSenderId: "381254097629",
+  appId: "1:381254097629:web:21b32c49f54c198b3d9934"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const authFirebase = firebase.auth;
+
+  const auth = useAuth(authFirebase);
 
   const openItem = useOpenItem();
   const orders = useOrders();
@@ -16,7 +33,7 @@ function App() {
   return (
     <>
       <GlobalStyle/>
-      <NavBar/>
+      <NavBar {...auth}/>
       <Order {...orders} {...openItem}/>
       <Menu {...openItem}/>
       { openItem.openItem && <ModalItem {...openItem} {...orders}/>}
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -39,20 +39,45 @@ const Login = styled.button`
    color: #ffffff;
    font-weight: 600;
    margin-top: 3px;
-   
-
-   
 `;
 
-export const NavBar = () => (
+const User =styled.div `
+   display: flex;
+   align-items: center;
+   text-align: center;
+`;
+
+const LogOut = styled.span`
+   font-size: 20px;
+   font-weight: 700;
+   cursor: pointer;
+   margin-right: 30px;
+`;
+
+const Figure = styled.figure`
+   margin: 0 30px; 
+`;
+
+export const NavBar = ({ authentication, logIn, logOut }) => (
    <NavBarStyled>
       <Logo>
          <ImgLogo src={logoImg} alt="лого" />
          <H1>MrDonald's</H1>
       </Logo>
-      <Login>
-         <img src={siginImg} alt="войти" />
-         <p>войти</p>
+      {authentication ? 
+      <User>
+         <Figure>
+            <img src={siginImg} alt={authentication.displayName} />
+            <figcaption>{authentication.displayName}</figcaption>
+         </Figure>
+         <LogOut title="Выйти" onClick={logOut}>X</LogOut>
+      </User> :
+      <Login onClick={logIn}>
+         <Figure>
+            <img src={siginImg} alt="войти" />
+            <figcaption>войти</figcaption>
+         </Figure>
       </Login>
+      }
    </NavBarStyled>
 );
