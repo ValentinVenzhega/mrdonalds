4203fb1374c0857ad587755cbb097ba893cbc74b
Valentin Venzhega <valek917@yandex.ru>
2021-04-20 18:08:35 +0300

    очищаем заказ после нажатия оформить и зменяем title при открытии модального окна

--- src/Components/Hooks/useAuth.js
+++ src/Components/Hooks/useAuth.js
@@ -20,7 +20,7 @@ export function useAuth(authFirebase) {
             setAuthentication(null);
          }
       });
-   }, [authentication]);
+   }, [auth, authentication]);
 
    return { authentication, logIn, logOut };
 }
--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -69,6 +69,7 @@ export const Order = ({ orders, setOrders, setOpenItem, authentication, logIn, f
          email: authentication.email,
          order: newOrder
       });
+      setOrders([]);
    };
 
    const deleteItem = index => {
